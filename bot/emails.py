63d97cd3cbd960337405381e5e367282c6475c04
RJ <public.jagpalrohan@gmail.com>
2021-09-19 23:44:44 +0100

    Create cogs

---
+++ bot/emails.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+
+class Emails(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def read_messages(self):
+        # This function will read messages from the inbox and return any relevant, unprocessed messages
+
+        # The below code is the original code from run.py
+        # This code will need to be adapted or completely re-written, it is simply here for reference
+        # The win32com library cannot be used, as it reads the local windows user's outlook details
+        # The hosted environment will not have this user set, therefore an alternative method needs to be found
+        """
+        outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+        inbox = outlook.GetDefaultFolder(6)
+        messages = inbox.Items
+        email = None
+        messageIter = 0
+        while email == None:
+            message = messages[messageIter]
+            if str(message.Sender) == "Simon Lett":
+                email = message
+            messageIter += 1
+        if email == None:
+            return
+        body_content = email.body
+        print(body_content)
+        upd_channel = await bot.get_channel(UPDATE_CHANNEL)
+        if body_content != upd_channel.lastmessage.content:
+            await upd_channel.send(body_content)"""
+
+        pass
---
+++ bot/twitter.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+class Twitter(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def read_tweets(self):
+        # This function will use Twitter API to read GH tweets
+        # Tweets will be posted to a Discord channel
+        pass
