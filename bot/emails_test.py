f06cd55c7c513351da9291f247377c5975be2171
Furri777 <tomwilkinson7@me.com>
2021-09-20 19:10:42 +0100

    Formatted with Black

--- bot/emails_test.py
+++ bot/emails_test.py
@@ -10,28 +10,28 @@ import os
 
 # Login to school email
 load_dotenv()
-user = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-mb = MailBox('mail.greenhead.ac.uk').login(user, password)
+user = os.environ.get("USER")
+password = os.environ.get("PASSWORD")
+mb = MailBox("mail.greenhead.ac.uk").login(user, password)
 
 
 # Move to the Bot folder, creates it if not present
-if not mb.folder.exists('INBOX/Bot'):
-    mb.folder.create('INBOX/Bot')
+if not mb.folder.exists("INBOX/Bot"):
+    mb.folder.create("INBOX/Bot")
 
-mb.folder.set('INBOX/Bot')
+mb.folder.set("INBOX/Bot")
 
 # Fetch emails in the folder
 msgs = mb.fetch()
 
 for msg in msgs:
     # Emails are recieved in html, so translate it into plaintext
-    temp_text = BS(msg.html, 'html.parser').get_text()
+    temp_text = BS(msg.html, "html.parser").get_text()
     # Remove unnecessary blank lines
-    text = ''
+    text = ""
     for line in temp_text.splitlines():
-        if line != '':
-            text += line + '\n'
+        if line != "":
+            text += line + "\n"
 
     # Deal with attachments
     atts = []
